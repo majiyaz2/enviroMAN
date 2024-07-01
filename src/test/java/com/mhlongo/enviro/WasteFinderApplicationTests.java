bb925cd3d78fe1e9a4411919ab4a8fc7229a4f97
Andile Mhlongo <majiyaz2@gmail.com>
2024-07-01 20:00:54 +0200

    Inital Commit

---
+++ src/test/java/com/mhlongo/enviro/WasteFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhlongo.enviro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
