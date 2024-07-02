734e48705701b1d51064767de9c47544483b6539
Andile Mhlongo <majiyaz2@gmail.com>
2024-07-02 10:00:31 +0200

    removed imports

--- src/main/java/com/mhlongo/enviro/controller/CategoryController.java
+++ src/main/java/com/mhlongo/enviro/controller/CategoryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.tokens.Token.ID;
+
 
 import com.mhlongo.enviro.model.CategoryModel;
 import com.mhlongo.enviro.repositories.CategoryRepository;
