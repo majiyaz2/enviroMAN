3234a556efc4f5259f552cc30574473dbd72ec8a
Andile Mhlongo <majiyaz2@gmail.com>
2024-07-03 06:37:48 +0200

    Added a put request to category

--- src/main/java/com/mhlongo/enviro/controller/CategoryController.java
+++ src/main/java/com/mhlongo/enviro/controller/CategoryController.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,25 +30,37 @@ public class CategoryController {
     private CategoryRepository categoryRepository;
     
     @GetMapping("category")
-    public List<CategoryModel> logCategory(Long id){
-         log.info("Category Endpoint"+ categoryRepository.findAll());
-         return categoryRepository.findAll();
+    public ResponseEntity<List<CategoryModel>> logCategory(Long id){
+        log.info("Category Endpoint"+ categoryRepository.findAll());
+        return ResponseEntity.ok(categoryRepository.findAll());
         
     }
 
     @GetMapping("category/{id}")
-    public Optional<CategoryModel> getCategoryName(@PathVariable Long id){
+    public ResponseEntity<Optional<CategoryModel>> getCategoryName(@PathVariable Long id){
         log.info("Category ID: "+Long.toString(id));
-        return  categoryRepository.findById(id);
+        return  ResponseEntity.ok(categoryRepository.findById(id));
         
     }
     
     @PostMapping("category/addCategory")
-    public CategoryModel addCategoy(@RequestBody CategoryModel category){
+    public ResponseEntity<CategoryModel> addCategoy(@RequestBody CategoryModel category){
         categoryRepository.save(category);
         categoryRepository.flush();
         log.info("Added Category ID: "+Long.toString(category.getID()));
-        return category;
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("category/{id}")
+    public ResponseEntity<CategoryModel> addCategoy(@PathVariable Long id,@RequestBody CategoryModel category){
+        CategoryModel categoryModel = categoryRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Item not found with id: " + id));
+        categoryModel.setDescription(category.getDescription());
+        categoryModel.setName(category.getName());
+        categoryModel.setRecyclable(category.getIsRecyclable());
+        CategoryModel updatedCategoryModel = categoryRepository.save(categoryModel);
+        log.info("Added Category ID: "+Long.toString(category.getID()));
+        return ResponseEntity.ok(updatedCategoryModel);
     }
     
     @DeleteMapping("category/{id}")
